c01caeb1eb86d7c835c3bcfc54c9ad0f6e6447d3
Rafí Mota <rafilourencom@gmail.com>
2023-07-18 14:38:28 -0300

    Cronometro Finalizado

--- MNM2_Cronometro/Program.cs
+++ MNM2_Cronometro/Program.cs
@@ -10,16 +10,34 @@
         Console.Clear();
         Console.WriteLine("Isso aqui é um cronômetro.");
         Console.Write("Informe o tempo que você quer contar: ");
-        int time = int.Parse(Console.ReadLine());
 
-        if (time != 0)
+        string data = Console.ReadLine().ToLower();
+        int time = int.Parse(data.Substring(0, data.Length - 1));
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+        switch (type)
         {
-            Start(time);
+            case 'm': PreStart(time * 60); break;
+            case 's': PreStart(time); break;
+            
+            default: Menu(); break;
         }
-        else
-        {
-            Menu();
-        }
+    }
+
+    static void PreStart(int time)
+    {
+        Console.Clear();
+        Console.WriteLine("Ready...");
+
+        Thread.Sleep(1000);
+        Console.WriteLine("Set...");
+        
+        Thread.Sleep(1000);
+        Console.WriteLine("Go!");
+        Thread.Sleep(500);
+
+
+        Start(time);
     }
 
     static void Start(int time)
